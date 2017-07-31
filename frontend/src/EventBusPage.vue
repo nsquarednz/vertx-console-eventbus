41d23c82c6ecf5e889e0ae6f61debb9ada9a1f09
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-31 17:04:29 -0400

    Greater visual distinction

--- frontend/src/EventBusPage.vue
+++ frontend/src/EventBusPage.vue
@@ -23,6 +23,10 @@
     background-color: #f1f1f1;
     background-image: linear-gradient(to bottom, #fafafa 0, #ededed 100%);
 
+    .title, .subtitle {
+        text-shadow: 0px 1px #fafafa;
+    }
+
     .title {
         display: inline;
     }
