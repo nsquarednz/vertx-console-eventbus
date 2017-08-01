2034f8fbb3eb82c879284907ec2a40a838dbc801
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-31 20:01:42 -0400

    Client side one second rate calculation

--- frontend/src/MonitoredHandler.vue
+++ frontend/src/MonitoredHandler.vue
@@ -29,9 +29,31 @@ import abbreviate from 'number-abbreviate';
 export default {
     props: {
         handler: Object
-    }, 
+    },
     beforeMount() {
         this.abbreviate = abbreviate;
+        this.prevCount = -1;
+        this.prevTime = -1;
+    },
+    computed: {
+        oneSecondRate() {
+            if (this.handler.data.oneSecondRate === undefined) {
+                let rate;
+                const currCount = this.handler.data.count;
+                const currTime = performance.now();
+                if (this.prevCount === -1) {
+                    rate = 0;
+                } else {
+                    const deltaT = (currTime - this.prevTime) / 1000;
+                    rate = Math.floor((this.handler.data.count - this.prevCount) / deltaT);
+                }
+                this.prevCount = currCount;
+                this.prevTime = currTime;
+                return rate;
+            } else {
+                return this.handler.data.oneSecondRate;
+            }
+        }
     }
 }
 </script>
